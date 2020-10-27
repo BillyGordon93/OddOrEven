bc2027aa54e5190b7036c4f5e6c42994d00dc049
Billy Gordon <Billywgordon93@gmail.com>
2020-10-27 14:35:13 -0500

    Finished Application

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Data.SqlTypes;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
 
 namespace OddOrEven
 {
@@ -6,7 +9,40 @@ namespace OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, what is your name?");
+            
+            string userName = Console.ReadLine();
+              
+            Console.WriteLine($"{userName}, please give me a number to check to see if it is even or odd.");
+            MyMethodOne();
         }
+        public static bool MyMethodOne()
+        {
+            int userNumber;
+           
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber % 2 == 0)
+            {
+                Console.WriteLine("Your number is even");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd!");
+                return false;
+            }
+                         
+            
+            
+
+            
+            
+
+        }
+	
+
+	}
+            
+
     }
-}
+
